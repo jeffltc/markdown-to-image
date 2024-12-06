c787576621961658a4560db7644b372529bf266c
Jeff Zhang <454342343@qq.com>
2024-12-07 07:30:35 +0800

    fix css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/markdown-to-poster/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
+  plugins: [],
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,10 @@ export default defineConfig({
   server: {
     host: 'localhost',
     port: 5173,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
   },
   optimizeDeps: {
     include: ['@monaco-editor/react']
