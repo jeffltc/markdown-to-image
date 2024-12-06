2b4d27f838be614741ca6c0ecd2d1044dd1adec1
Jeff Zhang <454342343@qq.com>
2024-12-06 09:44:32 +0800

    Build and update dist files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/markdown-to-poster/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
 
